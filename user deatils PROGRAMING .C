fea3a4db76dd67d7466f6e0581627b71491e065b
swaroopchanni76 <swaroopchanni76@gmail.com>
2025-11-18 15:52:45 +0530

    Create user deatils PROGRAMING .C

---
+++ user deatils PROGRAMING .C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int swaroop(){
+    char name,department;
+    int age;
+    printf("enter a name\n");
+    scanf("%s",&name);
+    printf("enter a department\n");
+    scanf("%s",&department);
+    printf("enter a age\n");
+    scanf("%d",&age);
+}
+int main() {      
+    swaroop();
+}
