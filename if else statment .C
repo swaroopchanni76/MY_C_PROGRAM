0dc89dd2e72291da1a82721dcd30efb6b8ac139c
swaroopchanni76 <swaroopchanni76@gmail.com>
2025-11-18 11:08:38 +0530

    Create if else statment .C

---
+++ if else statment .C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+        int a;
+        printf("enter the number");
+        scanf("%d\n",&a);
+        if (a>0){
+         printf ("number is positive");
+        }
+        else{
+            printf("number is negative");
+        }
+    }
