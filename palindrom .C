287f2c0667c549b5feedb82004323c238c312ab8
swaroopchanni76 <swaroopchanni76@gmail.com>
2025-11-20 10:48:45 +0530

    Create palindrom .C

---
+++ palindrom .C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int num=121,org,rev=0,rem;
+    org=num;
+    while(num!=0){
+        rem=num%10;
+        rev=rev*10+rem;
+        num=num/10;
+        
+    }
+    if(org==rev){
+        printf("num  is palindrome");
+    }
+    else{
+        printf("it is not a palindrome");
+    }
+}
